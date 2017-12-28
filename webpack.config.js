db9c0ab9cb3af08b5f073c43933044e9a7b7e211
Raluca <nicola.raluca@gmail.com>
2017-12-28 11:11:19 +0100

    Add Babel

--- webpack.config.js
+++ webpack.config.js
@@ -7,5 +7,20 @@ module.exports = {
   output: {
     filename: './dist/[name].bundle.js',
     libraryTarget: 'amd'
-  }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ["es2015"]
+          }
+        }
+      }
+    ]
+  },
+  devtool: 'source-map'
 }
