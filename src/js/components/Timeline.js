3f8b11c9ba1e5fab9ca09f63a6902a3900d0c8fa
Raluca <nicola.raluca@gmail.com>
2017-12-31 17:42:40 +0100

    Add mouse over tooltip

--- src/js/components/Timeline.js
+++ src/js/components/Timeline.js
@@ -4,7 +4,8 @@ require('../../style/timeline.scss');
 function createMovieItem(movie) {
   let template = document.createElement('template');
   template.innerHTML = `
-    <div class='movie-item' data-title='${movie.title}' data-id=${movie.director}>
+    <div class='movie-item' data-title='${movie.title}' data-director='${movie.director}'
+    data-id=${movie.id} data-year=${movie.year} data-actor='${movie.actor}'>
       <img src='./src/img/posters/${movie.image}' class='grayed-out'/>
       <p>${movie.year}</p>
     </div>
@@ -12,14 +13,33 @@ function createMovieItem(movie) {
   return template.content.firstElementChild;
 }
 
+function updateTooltipContent(data) {
+  document.querySelector('.tooltip').innerHTML = `<h1>${data.title}</h1>
+      <p>${data.year}</p>
+      <p>${data.actor}</p>
+      <p>${data.director}</p>`;
+}
+
+function displayTooltip(positionX) {
+  let tooltipElement = document.querySelector('.tooltip');
+  tooltipElement.classList.add('visible');
+  tooltipElement.style.left = positionX + 'px';
+}
+
 function handleEventsOnMouseOver(movieItem) {
   movieItem.addEventListener('mouseover', function (evt) {
-    console.log(evt.target);
+    let target = evt.target;
+    let image = target.getElementsByTagName('img')[0];
+    image.classList.add('selected');
+    updateTooltipContent(target.dataset);
+    displayTooltip(target.offsetLeft);
     evt.stopPropagation();
   });
 
   movieItem.addEventListener('mouseout', function (evt) {
-    console.log(evt.target);
+    let image = evt.target.getElementsByTagName('img')[0];
+    image.classList.remove('selected');
+    document.querySelector('.tooltip').classList.remove('visible');
     evt.stopPropagation();
   });
 }
