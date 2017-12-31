da26923b4274d89341f73c64b99d98cd7a6adc19
Raluca <nicola.raluca@gmail.com>
2017-12-31 16:56:53 +0100

    Remove b&w images and replace them with css filter

--- src/js/components/Timeline.js
+++ src/js/components/Timeline.js
@@ -5,19 +5,40 @@ function createMovieItem(movie) {
   let template = document.createElement('template');
   template.innerHTML = `
     <div class='movie-item' data-title='${movie.title}' data-id=${movie.director}>
-      <img src='./src/img/posters/${movie.image.replace('.png', 'bw.png')}'/>
+      <img src='./src/img/posters/${movie.image}' class='grayed-out'/>
       <p>${movie.year}</p>
     </div>
     `;
   return template.content.firstElementChild;
 }
 
+function handleEventsOnMouseOver(movieItem) {
+  movieItem.addEventListener('mouseover', function (evt) {
+    console.log(evt.target);
+    evt.stopPropagation();
+  });
+
+  movieItem.addEventListener('mouseout', function (evt) {
+    console.log(evt.target);
+    evt.stopPropagation();
+  });
+}
+
 
 export const Timeline = {
+
+  // initialize timeline
   init(container) {
+
     movieList.movies.forEach(function(movie) {
+
+      // create movie element
       let movieItem = createMovieItem(movie);
       container.appendChild(movieItem);
+
+      // add event listeners
+      handleEventsOnMouseOver(movieItem);
     });
+
   }
 };
