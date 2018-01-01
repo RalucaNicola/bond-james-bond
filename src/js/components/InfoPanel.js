f7747b22cff232b5ffc57d7b3333faec976b9f7a
Raluca <nicola.raluca@gmail.com>
2018-01-01 20:39:48 +0100

    Add legend for bond actors

--- src/js/components/InfoPanel.js
+++ src/js/components/InfoPanel.js
@@ -1,6 +1,9 @@
 import watch from 'redux-watch';
 import Typed from 'typed.js';
+import {visualizationChanged} from '../actions/actionUtils';
+import movieList from '../../data/movie-list.json';
 
+import colors from '../../style/colors.scss';
 require('../../style/info-panel.scss');
 
 function startTyping(container, text, onComplete) {
@@ -21,7 +24,10 @@ function startTyping(container, text, onComplete) {
 }
 
 function onMenuClick(menuItem, store) {
-  console.log(store.getState());
+  store.dispatch(visualizationChanged(menuItem.innerHTML));
+  let previous = document.getElementsByClassName('info-selected')[0];
+  previous.classList.remove('info-selected');
+  menuItem.classList.add('info-selected');
 }
 
 function initializeMenu(container, store) {
@@ -38,11 +44,35 @@ function initializeMenu(container, store) {
   });
 }
 
+function getUniqueActors(movies) {
+  let actors = [];
+  for (let i = 0; i < movies.length; i++) {
+    if (actors.indexOf(movies[i].actor) === -1) {
+      actors.push(movies[i].actor);
+    }
+  }
+  return actors;
+}
+
+function displayActors(container) {
+  let actors = getUniqueActors(movieList.movies);
+  console.log(movieList.movies);
+  actors.forEach((actor) => {
+    let actorTemplate = document.createElement('template');
+    actorTemplate.innerHTML = `<div>
+      <img class='bond-symbol' src='./src/img/bond-actor.png' style='border:3px solid ${colors['actors-' + actor.replace(' ','').toLowerCase()]}'>
+      <span>${actor}</span>
+    </div>`;
+    container.appendChild(actorTemplate.content.firstElementChild);
+  });
+}
+
 export const InfoPanel = {
   init(store) {
     this.container = document.getElementById('info-panel');
     this.title = document.getElementById('info-title');
     this.menu = document.getElementById('info-menu');
+    this.details = document.getElementById('info-details');
 
     let viewReadyWatcher = watch(store.getState, 'initialization.viewReady');
     store.subscribe(viewReadyWatcher(() => {
@@ -50,6 +80,13 @@ export const InfoPanel = {
         `Visualize James Bond's travels based on:`,
         () => { initializeMenu(this.menu, store); }
       );
+    }));
+
+    let visualizationChangeWatcher = watch(store.getState, 'visualization');
+    store.subscribe(visualizationChangeWatcher((value) => {
+      if (value.mode === 'Actor') {
+        displayActors(this.details);
+      }
     }));
   }
 };
